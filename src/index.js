3ba6b9d63e42ef3536133e4092a61c6b2adf5b81
Roger Mayer <rmayer1984@gmail.com>
2020-03-16 13:05:06 -0500

    converted jsx sytle

--- src/index.js
+++ src/index.js
@@ -4,8 +4,13 @@ import ReactDom from 'react-dom';
 
 // Create React Component
 const App = () =>{
-    return <div>Hi There!</div>;
-}
+    return (
+        <div>
+        <label htmlFor="name">Enter Name</label>
+        <input id="name" type="text"/>
+        <button style={{backgroundColor: 'blue', color:'white'}}>Submit</button>
+    </div>)
+};
 
 
 // Take React Component and show on screen
